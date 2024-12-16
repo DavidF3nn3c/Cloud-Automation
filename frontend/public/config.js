82839082f51ad1de831ad58eae52e1bbe696bd76
Fennec <cd03puma@gmail.com>
2024-12-16 22:57:31 +0100

    add backend url

--- frontend/public/config.js
+++
@@ -1,3 +0,0 @@
-window.configs = {
-    apiUrl: 'https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1', // Aquí va la URL de tu backend
-};
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-//const apiUrl = window?.configs?.apiUrl
-//  ? window.configs.apiUrl
-//  : "https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1";
-
-const apiUrl =
-  "https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1";
+const apiUrl = "/choreo-apis/cloudautomation/backend/v1";
 
 const api = axios.create({
-  baseURL: apiUrl,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
