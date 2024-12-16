bec4fa90578664f596ef4652b4a27d7bfbae223b
Fennec <cd03puma@gmail.com>
2024-12-16 16:37:04 +0100

    Final Debbug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = window?.configs?.apiUrl
-  ? window.configs.apiUrl
-  : "https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1";
+//const apiUrl = window?.configs?.apiUrl
+//  ? window.configs.apiUrl
+//  : "https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1";
+
+const apiUrl =
+  "https://96bb09cd-781e-4e9c-9f15-303d190391bc-dev.e1-eu-north-azure.choreoapis.dev/cloudautomation/backend/v1";
 
 const api = axios.create({
   baseURL: apiUrl,
