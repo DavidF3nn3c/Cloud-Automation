8bc681d029c03aef1142efd10f91962339293eb0
Fennec <cd03puma@gmail.com>
2024-12-16 23:45:42 +0100

    add backend url

---
+++ frontend/public/config.js
@@ -0,0 +1,3 @@
+window.configs = {
+  API_BASE_URL: "/choreo-apis/backend-3567783701:8000",
+};
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/backend-3567783701:8000";
+const apiUrl = window?.configs?.apiUrl
+  ? window.configs.apiUrl
+  : "/choreo-apis/backend-3567783701:8000";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: apiUrl,
 });
 
 api.interceptors.request.use(
